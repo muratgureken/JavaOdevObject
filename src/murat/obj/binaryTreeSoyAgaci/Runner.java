373f7fad213f430eb9952355c31ca76f9bb357a3
MGUREKEN <MGUREKEN@TOSHIBA_MGUREKEN>
2018-12-28 22:19:18 +0200

    28.12.2018 binary tree runner ekleme

--- src/murat/obj/binaryTreeSoyAgaci/Runner.java
+++ src/murat/obj/binaryTreeSoyAgaci/Runner.java
@@ -6,12 +6,19 @@ public class Runner {
 		BST aile= new BST();
 		Node ben = null;
 		
-		ben = aile.insert(ben, "Murat","",3);
-		ben = aile.insert(ben, "Emel","A",3);
-		ben = aile.insert(ben, "Turan","AB",3);
+		ben = aile.insert(ben, "Murat","",4);
+		ben = aile.insert(ben, "Emel","A",4);
+		ben = aile.insert(ben, "Turan","AB",4);
+		ben = aile.insert(ben, "Erol","B",4);
+		ben = aile.insert(ben, "Ayse","BA",4);
+		ben = aile.insert(ben, "Fatma","BAA",4);
+		ben = aile.insert(ben, "Mehmet","BAAB",4);
 		System.out.println(ben.data+" "+ben.adi);
 		System.out.println(ben.anne.data+" "+ben.anne.adi);
 		System.out.println(ben.anne.baba.data+" "+ben.anne.baba.adi);
+		System.out.println(ben.baba.data+" "+ben.baba.adi);
+		System.out.println(ben.baba.anne.anne.baba.data+" "+ben.baba.anne.anne.baba.adi);
+		
 	}
 
 }
